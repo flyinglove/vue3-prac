e6582436e890b2cab4ef0449acb96bbfebabdc5d
wangguanghui <wangguanghui1@guazi.com>
2020-05-15 18:56:35 +0800

    update

---
+++ src/libs/net.js
@@ -0,0 +1,5 @@
+
+class netWork {
+  constructor() {}
+
+}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import router from './routers'
 import App from './App.vue'
 
+import './libs/rem.js'
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
